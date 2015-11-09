b245e7488f26ac8aaf60236bc7a2385034e44ce7
Peng Wang <pengw@Pengw-MacBook-Pro.local>
2015-11-09 17:25:57 -0500

    add label for recording

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordAudio(sender: UIButton) {
+        print("button pressed")
+      
+      
+        
+    }
+    @IBAction func TestTouchDown(sender: UIButton) {
+        
+    }
 
+ 
+    
+    @IBAction func StopRecording(sender: UIButton) {
+    }
 }
 
